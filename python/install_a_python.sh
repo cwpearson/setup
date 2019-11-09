7bcd2cff970d0aa8f0a255c89d72ff96ea3905fb
Carl Pearson <carl.w.pearson@gmail.com>
2019-11-09 07:45:21 -0600

    improve examples

--- python/install_a_python.sh
+++ python/install_a_python.sh
@@ -10,16 +10,21 @@ pyenv version
 echo "using pip:"
 which pip
 
-
-mkdir -p python3.7.3
-cd python3.7.3
+# make sure python3 and python2 can be installed
 pyenv install 3.7.3
-pyenv local 3.7.3
-python3 --version
-which python3
-pipenv shell --python python3
+pyenv install 2.7.15
 
-which python
-python --version
-which pip
-pyenv version
+# running in /python2.7.15 would cause an assertion error
+mkdir -p /root/python3.7.3
+mkdir -p /root/python2.7.15
+
+# mess around with python3
+cd /root/python3.7.3
+pipenv run --python 3.7.3 python --version
+pipenv install networkx
+
+# mess around with python2
+cd /root/python2.7.15
+pipenv run --python 2.7.15 python --version
+pipenv install networkx
+
